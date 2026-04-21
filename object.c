0fd2b525977a268ddb121a2954165f9ee8f9a3f4
PREETHI-T0511 <preethitatikonda@gmail.com>
2026-04-21 14:44:34 +0530

    Phase 1: Implement object_read header parsing

--- object.c
+++ object.c
@@ -196,6 +196,23 @@ int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_
     }
     fclose(f);
 
+    uint8_t *null_byte = memchr(full_data, '\0', file_size);
+    if (!null_byte) {
+        free(full_data);
+        return -1;
+    }
+
+    if (strncmp((char *)full_data, "blob", 4) == 0) {
+        *type_out = OBJ_BLOB;
+    } else if (strncmp((char *)full_data, "tree", 4) == 0) {
+        *type_out = OBJ_TREE;
+    } else if (strncmp((char *)full_data, "commit", 6) == 0) {
+        *type_out = OBJ_COMMIT;
+    } else {
+        free(full_data);
+        return -1;
+    }
+
     free(full_data);
     return 0;
 }
